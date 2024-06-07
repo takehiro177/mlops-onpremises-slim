cea33d0e8b3ba24842ae648d64acfcf8eebbec65
Takehiro Ohashi <50972410+takehiro177@users.noreply.github.com>
2024-06-07 14:45:28 +0700

    docker images

---
+++ docker/airflow/start-airflow.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Move to the AIRFLOW HOME directory
+cd $AIRFLOW_HOME
+
+# Export environement variables
+export AIRFLOW__CORE__LOAD_EXAMPLES=False
+
+# Initiliase the metadatabase
+airflow db init
+
+# Create User
+airflow users create -e "mlops@mlops.com" -f "mlops" -l "mlops" -p "mlops" -r "Admin" -u "takehiro"
+
+# Run the scheduler in background
+airflow scheduler &> /dev/null &
+
+# Run the web sever in foreground (for docker logs)
+exec airflow webserver
