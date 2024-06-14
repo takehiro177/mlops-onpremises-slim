70e962a8a826efb5392762bdf878f4c598b2ddc4
Takehiro Ohashi <50972410+takehiro177@users.noreply.github.com>
2024-06-14 11:12:21 +0700

    add README.md

--- reset.sh
+++ reset.sh
@@ -5,13 +5,13 @@ docker system prune -f
 docker volume prune -f
 docker network prune -f
 
-# rm -rf ./mnt/postgres/*  #  Linux ONLY
-Remove-Item -Path ./mnt/postgres/* -Recurse  #  Windows PowerShell ONLY
+#rm -rf ./mnt/postgres/!(README.md)  #  linux ONLY
+Remove-Item -Path ./mnt/postgres/* -Exclude README.md -Recurse  #  Windows PowerShell ONLY
 
-# rm -rf ./mnt/airflow/logs/*  #  linux ONLY
-Remove-Item -Path ./mnt/airflow/logs/* -Recurse  #  Windows PowerShell ONLY
+# rm -rf ./mnt/airflow/logs/!(README.md)  #  linux ONLY
+Remove-Item -Path ./mnt/airflow/logs/* -Exclude README.md -Recurse  #  Windows PowerShell ONLY
 
-# rm -rf ./mnt/mlflow/*  #  linux ONLY
-Remove-Item -Path ./mnt/mlflow/* -Recurse  #  Windows PowerShell ONLY
+# rm -rf ./mnt/mlflow/!(README.md)  #  linux ONLY
+Remove-Item -Path ./mnt/mlflow/* -Exclude README.md -Recurse  #  Windows PowerShell ONLY
 
 docker rmi -f $(docker images -a -q)
